8351942e79ab6103fe1ed3a88b2c637fee54b209
claudiolammirato <claudio.lammirato@gmail.com>
2015-01-04 18:59:21 +0100

    First release

--- index.py
+++ index.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask import render_template
+from flask.ext.bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-	return "<h1>Hello Claudio!</h1>"
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
