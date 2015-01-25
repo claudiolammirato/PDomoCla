132c4a23d7283c0deced77cfe826748f1e8a5ed7
claudiolammirato <claudio.lammirato@gmail.com>
2015-01-25 16:48:19 +0100

    development
    0.1

---
+++ app/main/forms.py
@@ -0,0 +1,16 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Length
+
+
+class LoginForm(Form):
+    username = StringField('Username', validators=[Required(), Length(1, 16)])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Submit')
+
+class ChangeUser(Form):
+	username = StringField('Username', validators=[Required(), Length(1,16)])
+	password = PasswordField('Password', validators=[Required()])
+	passwordconfirm = PasswordField('Password Confirmation', validators=[Required()])
+	submit = SubmitField('Save')
---
+++ app/main/routes.py
@@ -0,0 +1,41 @@
+from flask import render_template, redirect, url_for, request
+from flask.ext.login import login_required, login_user, logout_user
+from ..models import User
+from . import main
+from .forms import LoginForm, ChangeUser
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.verify_password(form.password.data):
+            return redirect(url_for('main.login', **request.args))
+        login_user(user, form.remember_me.data)
+        return redirect(request.args.get('next') or url_for('main.index'))
+    return render_template('login.html', form=form)
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+@main.route('/temperature')
+@login_required
+def temperature():
+    return render_template('temperature.html')
+
+@main.route('/impostazioni', methods=['GET', 'POST'])
+@login_required
+def impostazioni():
+    form1 = ChangeUser()
+    return render_template('impostazioni.html', form1=form1)
